8745e79870f3c3fa9a3e86bfd93725a2dc8c38e5
Ann Ndiga <annndiga@gmail.com>
2023-07-02 16:16:12 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('#searchByID');
+      const movieDetails = document.querySelector('#movieDetails');
+      const title = movieDetails.querySelector('h4');
+      const summary = movieDetails.querySelector('p');
   
-}
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+})
+.catch(error => {
+    title.innerText = 'Movie Not Found';
+    summary.innerText = '';
+    console.error(error);
+  });
+});
+};
 
 document.addEventListener('DOMContentLoaded', init);
